f871ac6f28ce5bcb77677e0cd5b6c61ae8b9783d
zachary mines <zmines@wesleyan.edu>
2020-07-02 03:54:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,10 @@
 class Book 
+  attr_accessor :author, :page_count, :genre
   def initialize(title)
     @title = title 
   end 
   
-  attr_accessor :author, :page_count, :genre
+  
   
   def title 
     @title
